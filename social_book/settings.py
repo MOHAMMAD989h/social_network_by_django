6b076b76e9ae645f544a16fa64b57b98293569cb
Mohammad Asadpour <asadpour808@gmail.com>
2025-08-14 11:22:26 +0330

    some details

--- social_book/settings.py
+++ social_book/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-a52e0wwh+fpfi+7-bf4#the&f7&*%qp%fdgcjp%33**243&^m_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.168.1.100',
+    '127.0.0.1'
+                 ]
 
 
 # Application definition
